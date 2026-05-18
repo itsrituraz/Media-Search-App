2a59d362cc78ca00e56eed987ff1ee38263e6f3c
itsrituraz <itsriturazz@gmail.com>
2026-05-18 10:35:19 +0530

    Fixed API requests

--- src/api/mediaApi.js
+++ src/api/mediaApi.js
@@ -5,7 +5,7 @@ const PEXELS_KEY = import.meta.env.VITE_PEXELS_KEY
 const GIPHY_KEY = import.meta.env.VITE_GIPHY_KEY
 
 export async function fetchPhotos(query,page=1,per_page=20){
-  const res=await axios.get('https://api.unsplash.com/search/photos?page=1&query=${query}&per_page=16',{
+  const res=await axios.get(`https://api.unsplash.com/search/photos?page=1&query=${query}&per_page=16`,{
     params:{query,page,per_page},
     headers:{Authorization:`Client-ID ${UNSPLASH_KEY}`}
 
@@ -17,7 +17,7 @@ export async function fetchPhotos(query,page=1,per_page=20){
 
 
 export async function fetchVideos(query,page=1,per_page=15){
-  const res=await axios.get('https://api.pexels.com/videos/search',{
+  const res=await axios.get(`https://api.pexels.com/videos/search`,{
     params:{query,page,per_page},
     headers:{Authorization:PEXELS_KEY}
   })
@@ -25,7 +25,7 @@ export async function fetchVideos(query,page=1,per_page=15){
 }
 
 export async function fetchGIF(query,page=1,per_page=15){
-  const res=await axios.get('https://api.giphy.com/v1/gifs/search',{
+  const res=await axios.get(`https://api.giphy.com/v1/gifs/search`,{
     params:{
       api_key: GIPHY_KEY,
       q:query,
